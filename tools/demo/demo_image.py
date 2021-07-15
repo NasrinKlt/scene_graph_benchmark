51d2d0453e737e637f82ba09008812d5d8e31b1d
Esmaeil <NAK168@pitt.edu>
2021-07-14 21:08:45 -0700

    added json output to demo

--- tools/demo/demo_image.py
+++ tools/demo/demo_image.py
@@ -2,6 +2,7 @@
 
 import cv2
 import os.path as op
+import os
 import argparse
 import json
 
@@ -48,6 +49,20 @@ def postprocess_attr(dataset_attr_labelmap, label_list, conf_list):
         return [[], []]
 
 
+def convert_bbox_loc(loc):
+    x1 = loc[0]
+    y1 = loc[1]
+    x2 = loc[2]
+    y2 = loc[3]
+    w = abs(y1 - y2)
+    h = abs(x1 - x2)
+    if x1 < x2 and y1 < y2:
+        return x1, y1, w, h
+    elif x1 > x2 and y1 > y2:
+        return x2, y2, w, h
+    print('Error in converting bbox loc')
+
+
 def main():
     parser = argparse.ArgumentParser(description="Object Detection Demo")
     parser.add_argument("--config_file", metavar="FILE",
@@ -57,6 +72,8 @@ def main():
                         help="labelmap file to select classes for visualizatioin")
     parser.add_argument("--save_file", required=False, type=str, default=None,
                         help="filename to save the proceed image")
+    parser.add_argument("--output", required=False, type=str, default=".",
+                        help="path to output directory to save json files")
     parser.add_argument("--visualize_attr", action="store_true",
                         help="visualize the object attributes")
     parser.add_argument("--visualize_relation", action="store_true",
@@ -65,6 +82,7 @@ def main():
                         help="Modify config options using the command-line")
 
     args = parser.parse_args()
+
     cfg.set_new_allowed(True)
     cfg.merge_from_other_cfg(sg_cfg)
     cfg.set_new_allowed(False)
@@ -77,6 +95,9 @@ def main():
 
     output_dir = cfg.OUTPUT_DIR
     mkdir(output_dir)
+
+    json_output_dir = args.output
+    mkdir(json_output_dir)
 
     if cfg.MODEL.META_ARCHITECTURE == "SceneParser":
         model = SceneParser(cfg)
@@ -93,6 +114,15 @@ def main():
                                                 cfg.DATASETS.LABELMAP_FILE)
     assert dataset_labelmap_file
     dataset_allmap = json.load(open(dataset_labelmap_file, 'r'))
+    json.dump(dataset_allmap['label_to_idx'], op.join(output_dir, 'object_id.json'))
+    json.dump(dataset_allmap['attribute_to_idx'], op.join(output_dir, 'attr_id.json'))
+    json.dump(dataset_allmap['predicate_to_idx'], op.join(output_dir, 'predicate_id.json'))
+    with open(op.join(output_dir, 'rel_gqa.txt'), 'w') as f:
+        for i in range(len(dataset_allmap['idx_to_predicate'].keys()) + 1):
+            if i in dataset_allmap['idx_to_predicate'].keys():
+                pred = dataset_allmap['idx_to_predicate'][i]
+                f.write(f'{pred}\n')
+
     dataset_labelmap = {int(val): key
                         for key, val in dataset_allmap['label_to_idx'].items()}
     # visual_labelmap is used to select classes for visualization
@@ -110,67 +140,142 @@ def main():
         dataset_relation_labelmap = {
             int(val): key for key, val in
             dataset_allmap['predicate_to_idx'].items()}
+    scene_graphs = {}
+    concept = {}
+    all_data = {}
+    for folder in os.listdir(args.img_file):
+        if folder[0] != '.':
+            for file in os.listdir(op.join(args.img_file, folder)):
+                if file[0] != '.':
+                    sg = {}
+                    data = {
+                        "objects": [],
+                        "attributes": [],
+                        "relations": [],
+                        "bbox_cords": [],
+                        "bbox_scores": [],
+                    }
+                    concept_list = []
+                    img_id = f'{folder}/{file}'
+                    transforms = build_transforms(cfg, is_train=False)
+                    cv2_img = cv2.imread(op.join(args.img_file, img_id))
+                    h, w, _ = cv2_img.shape
+                    sg["width"] = w
+                    sg["height"] = h
+                    objects = {}
+                    dets = detect_objects_on_single_image(model, transforms, cv2_img)
+                    if isinstance(model, SceneParser):
+                        rel_dets = dets['relations']
+                        dets = dets['objects']
 
-    transforms = build_transforms(cfg, is_train=False)
-    cv2_img = cv2.imread(args.img_file)
-    dets = detect_objects_on_single_image(model, transforms, cv2_img)
+                    for i, obj in enumerate(dets):
+                        x, y, w, h = convert_bbox_loc(obj['rect'])
+                        obj_dict = {
+                            "label_id": obj["class"],
+                            "name": dataset_labelmap[obj["class"]],
+                            "score": obj["conf"],
+                            "relations": [],
+                            "attributes": [],
+                            "attributes_scores": [],
+                            "x": x,
+                            "y": y,
+                            "w": w,
+                            "h": h
+                        }
+                        data['bbox_cords'].append(obj['rect'])
+                        data['bbox_scores'].append(obj['conf'])
+                        if obj_dict['name'] not in concept_list:
+                            concept_list.append(obj_dict['name'])
+                        if obj_dict['name'] not in data['objects']:
+                            data['objects'].append(obj_dict['name'])
+                        if 'attr' in obj:
+                            attr_names, attr_scs = postprocess_attr(dataset_attr_labelmap, obj["attr"],
+                                                                             obj["attr_conf"])
+                            for i, attr_name in enumerate(attr_names):
+                                obj_dict['attributes'].append(attr_name)
+                                obj_dict['attributes_scores'].append(attr_scs[i])
+                                if attr_name not in concept_list:
+                                    concept_list.append(attr_name)
+                                if attr_name not in data['attributes']:
+                                    data['attributes'].append(attr_name)
+                        objects[img_id + '_' + str(i)] = obj_dict
 
-    if isinstance(model, SceneParser):
-        rel_dets = dets['relations']
-        dets = dets['objects']
+                    if cfg.MODEL.RELATION_ON:
+                        for rel in rel_dets:
+                            rel_dict = {
+                                "name": dataset_relation_labelmap[rel['class']],
+                                "predicate_id": rel['class'],
+                                "score": rel['conf'],
+                                "object": img_id + '_' + str(rel['subj_id'])
+                            }
+                            if rel_dict['name'] not in concept_list:
+                                concept_list.append(rel_dict['name'])
+                            if rel_dict['name'] not in data['relations']:
+                                data['relations'].append(rel_dict['name'])
+                            objects[img_id + '_' + str(rel['obj_id'])]['relations'].append(rel_dict)
 
-    for obj in dets:
-        obj["class"] = dataset_labelmap[obj["class"]]
-    if visual_labelmap is not None:
-        dets = [d for d in dets if d['class'] in visual_labelmap]
-    if cfg.MODEL.ATTRIBUTE_ON and args.visualize_attr:
-        for obj in dets:
-            obj["attr"], obj["attr_conf"] = postprocess_attr(dataset_attr_labelmap, obj["attr"], obj["attr_conf"])
-    if cfg.MODEL.RELATION_ON and args.visualize_relation:
-        for rel in rel_dets:
-            rel['class'] = dataset_relation_labelmap[rel['class']]
-            subj_rect = dets[rel['subj_id']]['rect']
-            rel['subj_center'] = [(subj_rect[0]+subj_rect[2])/2, (subj_rect[1]+subj_rect[3])/2]
-            obj_rect = dets[rel['obj_id']]['rect']
-            rel['obj_center'] = [(obj_rect[0]+obj_rect[2])/2, (obj_rect[1]+obj_rect[3])/2]
+                    for obj in dets:
+                        obj["class"] = dataset_labelmap[obj["class"]]
+                    if visual_labelmap is not None:
+                        dets = [d for d in dets if d['class'] in visual_labelmap]
+                    if cfg.MODEL.ATTRIBUTE_ON and args.visualize_attr:
+                        for obj in dets:
+                            obj["attr"], obj["attr_conf"] = postprocess_attr(dataset_attr_labelmap, obj["attr"], obj["attr_conf"])
+                    if cfg.MODEL.RELATION_ON and args.visualize_relation:
+                        for rel in rel_dets:
+                            rel['class'] = dataset_relation_labelmap[rel['class']]
+                            subj_rect = dets[rel['subj_id']]['rect']
+                            rel['subj_center'] = [(subj_rect[0]+subj_rect[2])/2, (subj_rect[1]+subj_rect[3])/2]
+                            obj_rect = dets[rel['obj_id']]['rect']
+                            rel['obj_center'] = [(obj_rect[0]+obj_rect[2])/2, (obj_rect[1]+obj_rect[3])/2]
 
 
-    rects = [d["rect"] for d in dets]
-    scores = [d["conf"] for d in dets]
-    if cfg.MODEL.ATTRIBUTE_ON and args.visualize_attr:
-        attr_labels = [','.join(d["attr"]) for d in dets]
-        attr_scores = [d["attr_conf"] for d in dets]
-        labels = [attr_label+' '+d["class"]
-                  for d, attr_label in zip(dets, attr_labels)]
-    else:
-        labels = [d["class"] for d in dets]
+                    rects = [d["rect"] for d in dets]
+                    scores = [d["conf"] for d in dets]
+                    if cfg.MODEL.ATTRIBUTE_ON and args.visualize_attr:
+                        attr_labels = [','.join(d["attr"]) for d in dets]
+                        attr_scores = [d["attr_conf"] for d in dets]
+                        print(f'attr labels: {attr_labels}')
+                        print(f'attr scores: {attr_scores}')
+                        labels = [attr_label+' '+d["class"]
+                                  for d, attr_label in zip(dets, attr_labels)]
+                    else:
+                        labels = [d["class"] for d in dets]
 
-    draw_bb(cv2_img, rects, labels, scores)
+                    draw_bb(cv2_img, rects, labels, scores)
 
-    if cfg.MODEL.RELATION_ON and args.visualize_relation:
-        rel_subj_centers = [r['subj_center'] for r in rel_dets]
-        rel_obj_centers = [r['obj_center'] for r in rel_dets]
-        rel_scores = [r['conf'] for r in rel_dets]
-        rel_labels = [r['class'] for r in rel_dets]
-        draw_rel(cv2_img, rel_subj_centers, rel_obj_centers, rel_labels, rel_scores)
+                    if cfg.MODEL.RELATION_ON and args.visualize_relation:
+                        rel_subj_centers = [r['subj_center'] for r in rel_dets]
+                        rel_obj_centers = [r['obj_center'] for r in rel_dets]
+                        rel_scores = [r['conf'] for r in rel_dets]
+                        rel_labels = [r['class'] for r in rel_dets]
+                        draw_rel(cv2_img, rel_subj_centers, rel_obj_centers, rel_labels, rel_scores)
 
-    if not args.save_file:
-        save_file = op.splitext(args.img_file)[0] + ".detect.jpg"
-    else:
-        save_file = args.save_file
-    cv2.imwrite(save_file, cv2_img)
-    print("save results to: {}".format(save_file))
+                    if not args.save_file:
+                        save_file = op.splitext(args.img_file)[0] + ".detect.jpg"
+                    else:
+                        save_file = args.save_file
+                    cv2.imwrite(save_file, cv2_img)
+                    print("save results to: {}".format(save_file))
 
-    # save results in text
-    if cfg.MODEL.ATTRIBUTE_ON and args.visualize_attr:
-        result_str = ""
-        for label, score, attr_score in zip(labels, scores, attr_scores):
-            result_str += label+'\n'
-            result_str += ','.join([str(conf) for conf in attr_score])
-            result_str += '\t'+str(score)+'\n'
-        text_save_file = op.splitext(save_file)[0] + '.txt'
-        with open(text_save_file, "w") as fid:
-            fid.write(result_str)
+                    # save results in text
+                    if cfg.MODEL.ATTRIBUTE_ON and args.visualize_attr:
+                        result_str = ""
+                        for label, score, attr_score in zip(labels, scores, attr_scores):
+                            result_str += label+'\n'
+                            result_str += ','.join([str(conf) for conf in attr_score])
+                            result_str += '\t'+str(score)+'\n'
+                        text_save_file = op.splitext(save_file)[0] + '.txt'
+                        with open(text_save_file, "w") as fid:
+                            fid.write(result_str)
+
+                    sg['objects'] = objects
+                    scene_graphs[img_id] = sg
+                    concept[img_id] = concept_list
+                    all_data[img_id] = data
+    json.dump(scene_graphs, op.join(output_dir, 'train_sceneGraphs.json'))
+    json.dump(concept, op.join(output_dir, 'images_objects.json'))
+    json.dump(all_data, op.join(output_dir, 'image_info.json'))
 
 
 if __name__ == "__main__":
